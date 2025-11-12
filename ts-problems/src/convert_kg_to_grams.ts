d872cb2e42e8e49ed93f70629f40d8aaab5c7188
israk03 <israkchowdhury03@gmail.com>
2025-11-12 20:50:41 +0600

    uses: type alias, union types, type narrowing

---
+++ ts-problems/src/convert_kg_to_grams.ts
@@ -0,0 +1,23 @@
+//! Create a function convertKgToG that takes a number or string (e.g. "5kg") and returns grams.
+
+type InputType = number | string;
+
+const convertKgToG = (input: InputType): number => {
+    if (typeof input === 'string'){
+        const numberValue = parseFloat(input);
+        if( isNaN(numberValue)){
+            throw new Error("Invalid input: Please provide a valid number or string representing kilograms.");
+        }
+        return numberValue * 1000;
+    } else if (typeof input === 'number'){
+        return input * 1000;
+    }
+
+    throw new Error("Invalid input type: Please provide a number or a string representing kilograms.");
+
+
+}
+
+
+console.log(convertKgToG(5));
+console.log(convertKgToG("5kg"));
