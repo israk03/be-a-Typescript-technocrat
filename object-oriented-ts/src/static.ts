4b312b4b06fec8220a07755321006b4ea7683f01
israk03 <israkchowdhury03@gmail.com>
2025-11-17 01:23:29 +0600

    learn: static

---
+++ object-oriented-ts/src/static.ts
@@ -0,0 +1,20 @@
+//static
+
+class Counter {
+    static count: number = 0;
+
+    static increment(){
+        return Counter.count += 1
+    }
+
+    static decrement(){
+        return Counter.count -= 1
+    }
+}
+
+// 
+
+console.log(Counter.increment())
+console.log(Counter.increment())
+console.log(Counter.increment())
+console.log(Counter.increment())
